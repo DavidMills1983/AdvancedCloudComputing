1621406aef0ec6288b74d088843c17911327502f
David Mills <millsdg@gmail>
2025-10-26 11:53:38 +0000

    Adding all files

---
+++ week4/mini-film-auth/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
+
+mongoose.connect(MURL, () => {
+    console.log('DB is connected')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running')
+})

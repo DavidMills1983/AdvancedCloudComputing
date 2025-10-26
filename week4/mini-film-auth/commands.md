6280f3849b0da123e615642559b896d73282faa8
David Mills <millsdg@gmail>
2025-10-26 15:25:56 +0000

    Final commit

--- week4/mini-film-auth/commands.md
+++ week4/mini-film-auth/commands.md
@@ -1,1 +1,11 @@
  npm install express nodemon mongoose dotenv body-parser
+
+* install joi package for validations
+ npm install joi
+
+* install bcryptjs for encryption/decrpytion
+ npm install bcryptjs
+
+* install the JWT package for generated auth-tokens
+
+npm jsonwebtoken
--- week4/mini-film-auth/routes/films.js
+++ week4/mini-film-auth/routes/films.js
@@ -2,14 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const Film = require('../models/Film')
+const verifyToken = require('../verifyToken')
 
-router.get('/', async(req,res)=>{
+router.get('/', verifyToken, async(req,res)=>{
     
     try {
         const films = await Film.find()
         res.send(films)
-    } catch(err) {message:err}
+    } catch(err) {
         res.status(400).send({message:err})
+    }
     
 })
 module.exports = router
